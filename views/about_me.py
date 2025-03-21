f61239038b584c6617b87457e3822e49258b65ac
felipejbs <felipejeronimobs@gmail.com>
2025-03-21 16:48:57 -0300

    atualizando about_me

--- views/about_me.py
+++ views/about_me.py
@@ -18,6 +18,7 @@ st.subheader("Experience & Qualifications", anchor=False)
 st.write(
     """
     - 2 anos de experiência na área de suporte e análise de dados
+    - Me especializando em Engenharia de dados
     - Sólida experiência prática e conhecimento em Python e SQL
     - Bom entendimento de engenharia e análise de dados
     - Boa capacidade de trabalho em equipe, proatividade e comunicação
@@ -29,9 +30,11 @@ st.write("\n")
 st.subheader("Hard Skills", anchor=False)
 st.write(
     """
-    - Programming: Python, SQL, VBA
-    - Data Visualization: Looker, PowerBi, Plotly
-    - Databases: MySQL, Postgres, MongoDB
+    - Programação: Python, SQL, Spark
+    - Visualização de dados: Looker, PowerBi, Plotly
+    - Banco de dados: MySQL, Postgres, MongoDB
     - Cloud: AWS
+    - Orquestração: Airflow
+    - Plataformas: Databricks, Snowflake
     """
 )
